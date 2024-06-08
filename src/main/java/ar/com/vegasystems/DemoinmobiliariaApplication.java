68dbaabb5407a8ef68d2a206990a02b4d6a09557
ArielAVega <141455765+ArielAVega@users.noreply.github.com>
2024-06-08 10:41:08 -0300

    Configuración inicial sin Bootstrap

---
+++ src/main/java/ar/com/vegasystems/DemoinmobiliariaApplication.java
@@ -0,0 +1,13 @@
+package ar.com.vegasystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoinmobiliariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoinmobiliariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/vegasystems/DemoinmobiliariaApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.vegasystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoinmobiliariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
